1f4be9754c26191eaeb85b62072d8ada463c0cca
Alyson <alysongamer35@gmail.com>
2020-11-20 19:42:09 -0300

    Aula 19-11

--- pages/Details.js
+++ pages/Details.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View,Text, StyleSheet } from 'react-native';
 import {useRoute} from '@react-navigation/native';
+import {Header} from 'react-native-elements';
+
 import GetPizzaHeader from '../components/GetPizzaHeader';
 
 const Details = () =>{
@@ -8,21 +10,18 @@ const Details = () =>{
     const pizza = route.params.props;
     console.log(pizza);
     return (
-        <View style={styles.container}>
+            <View style={styles.container}>
             <GetPizzaHeader/>
-                <View>
-                    <Text>{pizza.desc}</Text>
-                </View>
-        </View>
+              <View>
+                  <Text>{pizza.name}</Text>
+                  <Text>{pizza.desc}</Text>
+              </View>
+          </View>
 )}
 
 const styles = StyleSheet.create({
     container:{
       flex:1
-    },
-    logo:{
-      width:30,
-      height: 30
     },
   });
 
